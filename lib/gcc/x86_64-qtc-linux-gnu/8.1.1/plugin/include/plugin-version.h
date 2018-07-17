4785dcb4effb06651d365e40543da368ad2016eb
QUVNTNM-Jenkins <sudokamikaze@openmailbox.org>
2018-07-17 01:55:22 +0000

    Bump build of 17-07-2018

--- lib/gcc/x86_64-qtc-linux-gnu/8.1.1/plugin/include/plugin-version.h
+++ lib/gcc/x86_64-qtc-linux-gnu/8.1.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "8.1.1";
-static char datestamp[] = "20180714";
+static char datestamp[] = "20180717";
 static char devphase[] = "";
 static char revision[] = "";
 
--- x86_64-qtc-linux-gnu/include/c++/8.1.1/x86_64-qtc-linux-gnu/bits/c++config.h
+++ x86_64-qtc-linux-gnu/include/c++/8.1.1/x86_64-qtc-linux-gnu/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 8
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20180714
+#define __GLIBCXX__ 20180717
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
